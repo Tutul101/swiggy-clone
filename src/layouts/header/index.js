9a49611845b470b44398d29ef415870f882399d3
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-03-03 02:07:06 +0530

    add responsive header for both mobile and desktop

--- src/layouts/header/index.js
+++ src/layouts/header/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import SwiggyIcon from "../../assets/icons/swiggy-icon";
 
 import "./header.scss";
-
+import NavItem from "../../components/nav-item";
+import SearchIcon from "../../assets/icons/search-icon";
+import ProfileIcon from "../../assets/icons/profile-icon";
+import CartIcon from "../../assets/icons/cart-icon";
 const Header = () => {
   return (
     <div className="header-wrapper">
@@ -11,9 +14,18 @@ const Header = () => {
           <SwiggyIcon />
         </div>
         <ul className="header-menu">
-          <li>Option1</li>
-          <li>Option2</li>
-          <li>Option3</li>
+          <li>
+            <NavItem icon={SearchIcon} text="Search" />
+          </li>
+          <li>
+            <NavItem icon={ProfileIcon} text="Sign In" />
+          </li>
+          <li style={{ display: "flex" }}>
+            <div className="cart-wrapper">
+              <p>Cart</p>
+              <span>(0)</span>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
