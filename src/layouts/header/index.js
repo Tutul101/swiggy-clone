dac91bc89be5e7e09fb5ce275f72a9363e495f32
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-03-02 15:51:41 +0530

    basic header layout

--- src/layouts/header/index.js
+++ src/layouts/header/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import SwiggyIcon from "../../assets/icons/swiggy-icon";
+
+import "./header.scss";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="header-wrapper">
+      <div className="header">
+        <div className="swiggy-icon">
+          <SwiggyIcon />
+        </div>
+        <ul className="header-menu">
+          <li>Option1</li>
+          <li>Option2</li>
+          <li>Option3</li>
+        </ul>
+      </div>
     </div>
   );
 };
