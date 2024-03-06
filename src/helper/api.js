42863aff15c7e5c09380d4d292f9eefe7ae5182b
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-03-06 17:18:42 +0530

    add responsive cuisine card

--- src/helper/api.js
+++ src/helper/api.js
@@ -1,7 +1,7 @@
 export const getHomePageData = async () => {
   try {
     const data = await fetch(
-      `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D23.022505%26lng%3D72.5713621%26page_type%3DDESKTOP_WEB_LISTING`
+      `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D22.5900724%26lng%3D88.41100039999999%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING`
     );
     const resData = await data.json();
     return resData;
--- src/layouts/hero-carousel1/index.js
+++ src/layouts/hero-carousel1/index.js
@@ -1,8 +1,36 @@
 import React from "react";
+import _get from "lodash/get";
+
+import CusineCard from "../../components/cuisine-card";
 
 const HeroCarousel1 = ({ cards }) => {
   console.log("Carousel Cards", cards);
-  return <div>Hero Carouesel</div>;
+  const itemCards = _get(cards, ["imageGridCards", "info"], []);
+  console.log("image cards", itemCards);
+  return (
+    <div>
+      <CusineCard
+        imageUrl={itemCards && itemCards[0] && itemCards[0].imageId}
+        alt={itemCards && itemCards[0] && itemCards[0].action.text}
+      />
+      <CusineCard
+        imageUrl={itemCards && itemCards[1] && itemCards[1].imageId}
+        alt={itemCards && itemCards[0] && itemCards[0].action.text}
+      />
+      <CusineCard
+        imageUrl={itemCards && itemCards[2] && itemCards[2].imageId}
+        alt={itemCards && itemCards[0] && itemCards[0].action.text}
+      />
+      <CusineCard
+        imageUrl={itemCards && itemCards[3] && itemCards[3].imageId}
+        alt={itemCards && itemCards[0] && itemCards[0].action.text}
+      />
+      <CusineCard
+        imageUrl={itemCards && itemCards[4] && itemCards[4].imageId}
+        alt={itemCards && itemCards[0] && itemCards[0].action.text}
+      />
+    </div>
+  );
 };
 
 export default HeroCarousel1;
