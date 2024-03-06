fc3de45b6a8b8ad8057303703af0a31f555da9a9
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-03-06 20:43:10 +0530

    add different hero caraousel for dektop and mobile

--- src/layouts/hero-carousel1/index.js
+++ src/layouts/hero-carousel1/index.js
@@ -2,27 +2,59 @@ import React from "react";
 import _get from "lodash/get";
 
 import CusineCard from "../../components/cuisine-card";
-import Caraousel from "../../components/common/caraousel";
+import "./hero-carousel1.scss";
+import Headline from "../../components/headline";
 
 const HeroCarousel1 = ({ cards }) => {
   console.log("Carousel Cards", cards);
   const itemCards = _get(cards, ["imageGridCards", "info"], []);
   console.log("image cards", itemCards);
+  if (itemCards.length === 0) {
+    return null;
+  }
+  const headline = _get(cards, ["header", "title"], "");
+
   return (
-    <div>
-      <div style={{ fontSize: "40px" }}>
-        <Caraousel>
-          {itemCards &&
-            itemCards.map((cardItem) => {
-              const { imageId, action } = cardItem;
+    <div className="hero-caraousel-wrapper">
+      <div className="hero-caraousel-title-wrapper">
+        <Headline title={headline} />
+      </div>
+      <div className="carousel-desktop-wrapper">
+        {itemCards &&
+          itemCards.map((cardItem) => {
+            const { imageId, action } = cardItem;
+            return (
+              <CusineCard
+                imageUrl={cardItem && imageId}
+                alt={cardItem && action.text}
+                key={imageId}
+              />
+            );
+          })}
+      </div>
+      <div className="carousel-mobile-wrapper">
+        {itemCards &&
+          itemCards
+            .reduce((chunks, cardItem, index) => {
+              if (index % 2 === 0) {
+                chunks.push(itemCards.slice(index, index + 2));
+              }
+              return chunks;
+            }, [])
+            .map((chunk, index) => {
               return (
-                <CusineCard
-                  imageUrl={cardItem && imageId}
-                  alt={cardItem && action.text}
-                />
+                <div className="double-cusine" key={index}>
+                  <CusineCard
+                    imageUrl={chunk[0].imageId}
+                    alt={chunk[0].action.text}
+                  />
+                  <CusineCard
+                    imageUrl={chunk[1].imageId}
+                    alt={chunk[1].action.text}
+                  />
+                </div>
               );
             })}
-        </Caraousel>
       </div>
     </div>
   );
