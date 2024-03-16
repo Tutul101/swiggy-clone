c35323520d2b9a2a48e9fcb30e83c6a26b0c6651
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-03-16 22:03:35 +0530

    add restraunt list component

---
+++ src/components/restraunt-list/index.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import _get from "lodash/get";
+import Headline from "../headline";
+import { Link } from "react-router-dom";
+import RestrauntCard from "../restraunt-card";
+import "./restraunt-list.scss";
+
+const RestrauntList = ({ restrauntList }) => {
+  console.log("restrauntList", restrauntList);
+  const sectionTitle = _get(restrauntList, ["0", "card", "card", "title"], "");
+  const gridItems = _get(
+    restrauntList,
+    ["2", "card", "card", "gridElements", "infoWithStyle", "restaurants"],
+    []
+  );
+  console.log("grid items", gridItems);
+  const [restraunts, setRestraunts] = useState([]);
+  useEffect(() => {
+    setRestraunts(gridItems);
+  }, [restrauntList]);
+  return (
+    <div className="restraunt-list-wrapper">
+      <Headline title={sectionTitle} />
+      <div className="restraunt-container">
+        {restraunts.map((restraunt) => {
+          const { cta, info } = restraunt;
+          const restrauntLink = cta && cta.link;
+          const baseUrl = "https://www.swiggy.com";
+          const slug = restrauntLink.replace(baseUrl, "");
+          const deliveryTime = info.sla.slaString;
+          return (
+            <Link to={`${slug}`} key={info.id}>
+              <RestrauntCard
+                image={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${info.cloudinaryImageId}`}
+                name={info.name}
+                rating={info.avgRating}
+                deliveryTime={deliveryTime}
+                cuisines={info.cuisines}
+                location={info.areaName}
+              />
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default RestrauntList;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,7 @@ import _get from "lodash/get";
 import { getHomePageData } from "../../helper/api";
 import HeroCarousel1 from "../../layouts/hero-carousel1";
 import HeroCarousel2 from "../../layouts/hero-caaousel2";
+import RestrauntList from "../../components/restraunt-list";
 
 const Home = () => {
   const [homeData, setHomeData] = useState([]);
@@ -26,11 +27,14 @@ const Home = () => {
   if (error) {
     <h1>Some thing went wrong</h1>;
   }
+
   return (
     <div>
       <HeroCarousel1 cards={homeData[0] && homeData[0].card.card} />
 
       <HeroCarousel2 cards={homeData[1] && homeData[1].card.card} />
+
+      <RestrauntList restrauntList={homeData.slice(2, 5)} />
       {/* <h1>Home</h1> */}
     </div>
   );
