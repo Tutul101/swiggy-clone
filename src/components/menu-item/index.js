afa876955f3ca2bbd7f60f2c1080288c75c4b43f
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-04-03 09:15:37 +0530

    add menu list drop down

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -2,27 +2,22 @@ import React from "react";
 import RatingIcon from "../../assets/icons/rating-icon";
 
 import "./menu-item.scss";
-const MenuItem = () => {
+const MenuItem = ({ name, price, description, imageId }) => {
   const BaseImage =
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
   return (
     <div className="menu-item-wrapper">
       <div className="menu-item-info">
         <div className="menu-item-name">
-          <div className="dish-name">Garlic Breadsticks</div>
-          <div className="dish-price">₹109</div>
+          <div className="dish-name">{name}</div>
+          <div className="dish-price">₹{price}</div>
         </div>
-        {/* <div className="menu-item-rating-wrapper">
-           <RatingIcon /> 
-          4.5(331)
-        </div> */}
-        <div className="menu-item-description">
-          Baked to perfection. Your perfect pizza partner! Tastes best with dip
-        </div>
+
+        <div className="menu-item-description">{description}</div>
       </div>
 
       <div className="menu-item-image-wrapper">
-        <img src={`${BaseImage}77b13d58799f70f670be31e6fe53374e`} />
+        <img src={`${BaseImage}${imageId}`} />
         <button type="button" className="add-btn">
           ADD
         </button>
--- src/pages/restraunt-page/index.js
+++ src/pages/restraunt-page/index.js
@@ -5,21 +5,32 @@ import { useParams } from "react-router-dom";
 import RestrauntPageHeader from "../../components/restraunt-page-header";
 import MenuItem from "../../components/menu-item";
 import useLoadRestrauntData from "../../utils/useLoadRestrauntData";
+import MenuList from "../../components/menu-list";
 
 const RestrauntPage = () => {
   const { resId } = useParams();
   const { data: restrauntInfo } = useLoadRestrauntData(resId, []);
-  console.log("restrauntInfo", restrauntInfo);
+  console.log("restrauntInfo", restrauntInfo[4]);
 
   if (restrauntInfo.length === 0) {
     return <h1>Loading...</h1>;
   }
 
+  const menuList = _get(
+    restrauntInfo,
+    [4, "groupedCard", "cardGroupMap", "REGULAR", "cards"],
+    []
+  );
+
+  console.log("menuList", menuList);
   return (
     <div>
       <RestrauntPageHeader restrauntDetails={restrauntInfo[2]} />
-      <div style={{ marginTop: "2rem" }}>
-        <MenuItem />
+
+      <div>
+        {menuList.slice(1, menuList.length - 2).map((item, index) => {
+          return <MenuList menuSection={item} key={index} />;
+        })}
       </div>
     </div>
   );
