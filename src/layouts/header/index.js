f3d6edf1c0d7055c9e8bc90bd51189d2786cd978
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-04-09 22:30:03 +0530

    add links to the header

--- src/layouts/header/index.js
+++ src/layouts/header/index.js
@@ -1,24 +1,31 @@
 import React from "react";
-import SwiggyIcon from "../../assets/icons/swiggy-icon";
 
-import "./header.scss";
+import { Link } from "react-router-dom";
+
+import SwiggyIcon from "../../assets/icons/swiggy-icon";
 import NavItem from "../../components/nav-item";
 import SearchIcon from "../../assets/icons/search-icon";
 import ProfileIcon from "../../assets/icons/profile-icon";
+
+import "./header.scss";
 
 const Header = () => {
   return (
     <div className="header-wrapper">
       <div className="header">
-        <div className="swiggy-icon">
+        <Link to="/" className="swiggy-icon">
           <SwiggyIcon />
-        </div>
+        </Link>
         <ul className="header-menu">
           <li>
-            <NavItem icon={SearchIcon} text="Search" />
+            <Link to="search">
+              <NavItem icon={SearchIcon} text="Search" />
+            </Link>
           </li>
           <li>
-            <NavItem icon={ProfileIcon} text="Sign In" />
+            <Link to="sign-in">
+              <NavItem icon={ProfileIcon} text="Sign In" />
+            </Link>
           </li>
           <li style={{ display: "flex" }}>
             <div className="cart-wrapper">
